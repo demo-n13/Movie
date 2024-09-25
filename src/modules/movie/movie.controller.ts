312a7a3ea46911a7ff204dddd2da457725b7734a
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2024-09-25 15:28:42 +0500

    add custom parseint pipe

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  // ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { MovieService } from './movie.service';
-import { CreateMovieDto } from './dtos';
+import { CreateMovieDto, GetMoviesQueriesDto } from './dtos';
+import { ParseIntCustomPipe } from '@pipes';
 // import { CreateMovieResponse } from './interfaces';
 
 @Controller({
@@ -11,13 +22,15 @@ export class MovieController {
 
   @Get('/')
   @HttpCode(HttpStatus.ACCEPTED)
-  async getMovies(@Query() queries: Record<string, string>): Promise<any[]> {
+  async getMovies(@Query() queries: GetMoviesQueriesDto): Promise<any[]> {
     return await this.movieService.getAllMovies(queries);
   }
 
   @Get('/:movieId')
-  async getSingleMovie(@Param('movieId') movieId: string): Promise<any> {
-    return await this.movieService.getSingleMovie(+movieId);
+  async getSingleMovie(
+    @Param('movieId', new ParseIntCustomPipe(13)) movieId: number,
+  ): Promise<any> {
+    return await this.movieService.getSingleMovie(movieId);
   }
 
   @Post('/add')
